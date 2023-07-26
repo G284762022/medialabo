f4397a81e0fb94763737480a1dd39daee85a1114
G284762022 <228476@st.takushoku-u.ac.jp>
2023-07-26 22:48:44 +0900

    7/26

---
+++ weather.js
@@ -0,0 +1,66 @@
+let data = {
+  "coord": {
+    "lon": 116.3972,
+    "lat": 39.9075
+  },
+  "weather": [
+    {
+      "id": 803,
+      "main": "Clouds",
+      "description": "曇りがち",
+      "icon": "04d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 9.94,
+    "feels_like": 8.65,
+    "temp_min": 9.94,
+    "temp_max": 9.94,
+    "pressure": 1022,
+    "humidity": 14,
+    "sea_level": 1022,
+    "grnd_level": 1016
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 2.65,
+    "deg": 197,
+    "gust": 4.84
+  },
+  "clouds": {
+    "all": 53
+  },
+  "dt": 1646542386,
+  "sys": {
+    "type": 1,
+    "id": 9609,
+    "country": "CN",
+    "sunrise": 1646520066,
+    "sunset": 1646561447
+  },
+  "timezone": 28800,
+  "id": 1816670,
+  "name": "北京市",
+  "cod": 200
+};
+
+////////// 課題3-2 ここからプログラムを書こう
+console.log("緯度: " + data.coord.lat);
+console.log("経度: " + data.coord.lon);
+console.log("天気: " + data.weather[0].description);
+console.log("最低気温: " + data.main.temp_min + " °C");
+console.log("最高気温: " + data.main.temp_max + " °C");
+console.log("湿度: " + data.main.humidity + " %");
+console.log("風速: " + data.wind.speed + " m/s");
+console.log("風向: " + data.wind.deg);
+console.log("都市名: " + data.name);
+
+///4-2
+
+let a1 = document.querySelector('button#miru');
+    a1.addEventListener('click', tenki);
+
+function tenki(){
+    
+}
